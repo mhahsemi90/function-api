cd69743cdf11aaad95960099bc104ee0e4d7f81f
mhashrmi90 <mhashemi90@gmail.com>
2025-04-26 18:34:40 +0330

    change calculation input structure and some refactor

--- src/main/java/org/example/functionapi/FunctionDto.java
+++ src/main/java/org/example/functionapi/FunctionDto.java
@@ -1,11 +1,14 @@
 package org.example.functionapi;
 
 import java.util.Map;
+import java.util.Set;
 
 public class FunctionDto {
     private String id;
     private String functionName;
+    private Integer functionLevel;
     private Map<String, Object> argumentMap;
+    private Set<String> outPutParameterCodeSet;
 
     public String getId() {
         return id;
@@ -23,11 +26,27 @@ public class FunctionDto {
         this.functionName = functionName;
     }
 
+    public Integer getFunctionLevel() {
+        return functionLevel;
+    }
+
+    public void setFunctionLevel(Integer functionLevel) {
+        this.functionLevel = functionLevel;
+    }
+
     public Map<String, Object> getArgumentMap() {
         return argumentMap;
     }
 
     public void setArgumentMap(Map<String, Object> argumentMap) {
         this.argumentMap = argumentMap;
+    }
+
+    public Set<String> getOutPutParameterCodeSet() {
+        return outPutParameterCodeSet;
+    }
+
+    public void setOutPutParameterCodeSet(Set<String> outPutParameterCodeSet) {
+        this.outPutParameterCodeSet = outPutParameterCodeSet;
     }
 }
